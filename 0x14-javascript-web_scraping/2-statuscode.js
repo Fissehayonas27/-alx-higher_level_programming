7522fe55b60403432d913eeb22ff0454e5f503f4
FissehaYonas27 <fissehayonas444@gmail.com>
2022-10-18 20:35:38 +0300

    New commitment

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+// A script that display the status code of a GET request
 
-const request = require('request');
-const URL = process.argv[2];
-
-request(URL, function (err, response) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code: ' + response.statusCode);
-  }
+const args = process.argv;
+let request = require('request');
+request(args[2], function (error, response, body) {
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+  } else console.log('code:', response && response.statusCode);
 });
